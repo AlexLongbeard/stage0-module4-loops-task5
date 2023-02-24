fe7be5bc0fe39d180253d96d0bb7f50500f4de2f
AlexLongbeard <AGLBRDK.KR@gmail.com>
2023-02-24 22:05:31 +0600

    done 3 of 5

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        for (int i = 1; i <= sideLength; i++) {
+            if (i==(sideLength-1)/2) {
+                for (int j = 1; j <= (sideLength-1); j++) {
+                        System.out.print("8");
+                    }
+                System.out.println("8");
+            }
+            else {
+                for (int j = 1; j <= (sideLength-1); j++) {
+                    if (j==(sideLength-1)/2) {
+                    System.out.print("8");
+                }
+                    else {
+                        System.out.print(" ");
+                    }
+                    System.out.println(" ");
+            }
 
+                }
+            }
+        }
     }
-}
+
